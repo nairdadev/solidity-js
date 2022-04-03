2a433939b746df51cf59376618a5af0591703e92
nairdadev <adrian.cano.g@gmail.com>
2022-04-03 15:27:37 -0300

    added event and emit

--- contracts/TaskContract.sol
+++ contracts/TaskContract.sol
@@ -12,15 +12,15 @@ contract TasksContract {
         uint256 createdAt;
     }
 
-    // event TaskCreated(
-    //     uint256 id,
-    //     string title,
-    //     string description,
-    //     bool done,
-    //     uint256 createdAt
-    // );
-    // event TaskToggledDone(uint256 id, bool done);
-    //
+    event TaskCreated(
+        uint256 id,
+        string title,
+        string description,
+        bool done,
+        uint256 createdAt
+    );
+    event TaskToggledDone(uint256 id, bool done);
+
     mapping(uint256 => Task) public tasks;
 
     constructor() {
@@ -38,19 +38,19 @@ contract TasksContract {
            false,
             block.timestamp
         );
-        // emit TaskCreated(
-        //     tasksCounter,
-        //     _title,
-        //     _description,
-        //     false,
-        //     block.timestamp
-        // );
+        emit TaskCreated(
+            tasksCounter,
+            _title,
+            _description,
+            false,
+            block.timestamp
+        );
     }
 
     function toggleDone(uint256 _id) public {
         Task memory _task = tasks[_id];
         _task.done = !_task.done;
         tasks[_id] = _task;
-        // emit TaskToggledDone(_id, _task.done);
+        emit TaskToggledDone(_id, _task.done);
     }
 } 
